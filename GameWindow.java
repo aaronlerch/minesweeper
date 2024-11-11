ecc931d845d673f7c326bd8b9973e6b4c1572cab
Aaron Lerch <aaronlerch@gmail.com>
2024-11-11 18:16:25 -0500

    Wire up menus to support selecting a game mode

---
+++ GameWindow.java
@@ -0,0 +1,69 @@
+import java.awt.event.*;
+import javax.swing.*;
+
+public class GameWindow extends JFrame {
+    private MineFieldView _currentGame;
+
+    public GameWindow(String title) {
+        super(title);
+
+        _currentGame = null;
+    }
+
+    public void startDefaultGame() {
+        startGame(GameMode.REGULAR);
+    }
+
+    public void startGame(GameMode gameMode) {
+        // First, remove any existing component
+        if (_currentGame != null) {
+            remove(_currentGame);
+        }
+        
+        var gridSize = switch (gameMode) {
+            case GameMode.EASY -> 10;
+            case GameMode.REGULAR -> 20;
+            case GameMode.HARD -> 30;
+            default -> 20;
+        };
+
+        var mf = new MineField(gridSize, gridSize);
+        _currentGame = new MineFieldView(mf);
+
+        add(_currentGame);
+        pack();
+
+        _currentGame.requestFocusInWindow();
+    }
+
+    public void setUpMenus() {
+        var menu = new JMenuBar();
+
+        var file = new JMenu("File");
+        var newGame = new JMenu("New Game");
+
+        var easyGame = new JMenuItem("Easy");
+        easyGame.addActionListener(e -> startGame(GameMode.EASY));
+
+        var regularGame = new JMenuItem("Regular");
+        regularGame.addActionListener(e -> startGame(GameMode.REGULAR));
+
+        var hardGame = new JMenuItem("Hard");
+        hardGame.addActionListener(e -> startGame(GameMode.HARD));
+
+        var exit = new JMenuItem(new AbstractAction("Exit") {
+            public void actionPerformed(ActionEvent ae) {
+                System.exit(0);
+            }
+        });
+
+        menu.add(file);
+            file.add(newGame);
+                newGame.add(easyGame);
+                newGame.add(regularGame);
+                newGame.add(hardGame);
+            file.add(exit);
+
+        setJMenuBar(menu);
+    }
+}
--- Main.java
+++ Main.java
@@ -2,27 +2,12 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        var window = new JFrame("MINESWEEPER");
+        var window = new GameWindow("MINE SWEEPER");
         window.setSize(800, 800);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        var gridSize = 30;
-        // var width = 10;
-        // var height = 10;
-        var mf = new MineField(gridSize, gridSize);
-        var view = new MineFieldView(mf);
-
-        // var menu = new JMenuBar();
-        // var file = new JMenu("File");
-        // var exit = new JMenuItem()
-        // menu.add(exit);
-
-
-        window.add(view);
-        //window.setJMenuBar(menu);
-        window.pack();
-
         window.setVisible(true);
-        view.requestFocusInWindow();
+
+        window.setUpMenus();
+        window.startDefaultGame();
     }
 }
