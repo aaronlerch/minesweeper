8f59dbb8b7dd7e0d43990ce2b54f93281b8fbce0
Aaron Lerch <aaronlerch@gmail.com>
2024-11-11 15:14:43 -0500

    Starting to set the stage for nenus

--- Main.java
+++ Main.java
@@ -1,5 +1,3 @@
-import java.awt.KeyboardFocusManager;
-
 import javax.swing.*;
 
 public class Main {
@@ -8,12 +6,20 @@ public class Main {
         window.setSize(800, 800);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        var width = 10;
-        var height = 10;
-        var mf = new MineField(width, height);
+        var gridSize = 30;
+        // var width = 10;
+        // var height = 10;
+        var mf = new MineField(gridSize, gridSize);
         var view = new MineFieldView(mf);
 
+        // var menu = new JMenuBar();
+        // var file = new JMenu("File");
+        // var exit = new JMenuItem()
+        // menu.add(exit);
+
+
         window.add(view);
+        //window.setJMenuBar(menu);
         window.pack();
 
         window.setVisible(true);
